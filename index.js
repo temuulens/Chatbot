40909834e3fbbb50eabb30c1e101ed8c6510bd2d
Temuulen Sodnom <Temuulen Sodnom>
2016-12-21 20:49:30 +0800

    mid -> comment

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook/', function (req, res) {
         let senderID = event.sender.id
         var message = event.message;
 
-        var messageId = message.id;
+        // var messageId = message.mid;
 
         var messageText = message.text;
         var messageAttachments = message.attachments;
